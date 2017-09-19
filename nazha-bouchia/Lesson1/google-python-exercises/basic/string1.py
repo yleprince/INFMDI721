20a68d51a9e359c8571ae61cec57992984db5678
Nazha Bouchia <nbouchia@crdn-06.enst.fr>
2017-09-19 16:45:08 +0200

    feat: [Lesson1 - Google Python Exercices][string1.py completed]

--- nazha-bouchia/Lesson1/google-python-exercises/basic/string1.py
+++ nazha-bouchia/Lesson1/google-python-exercises/basic/string1.py
@@ -25,7 +25,10 @@
 # and donuts(23) returns 'Number of donuts: many'
 def donuts(count):
   # +++your code here+++
-  return
+  donuts_count = 'Number of donuts: '
+  if count<10 : donuts_count = donuts_count + str(count)
+  else : donuts_count = donuts_count + 'many'
+  return donuts_count
 
 
 # B. both_ends
@@ -34,8 +37,10 @@ def donuts(count):
 # so 'spring' yields 'spng'. However, if the string length
 # is less than 2, return instead the empty string.
 def both_ends(s):
-  # +++your code here+++
-  return
+  both_ends_result = ''
+  if len(s) > 2 :
+      both_ends_result = s[:2]+s[-2:]
+  return both_ends_result
 
 
 # C. fix_start
@@ -48,8 +53,7 @@ def both_ends(s):
 # Hint: s.replace(stra, strb) returns a version of string s
 # where all instances of stra have been replaced by strb.
 def fix_start(s):
-  # +++your code here+++
-  return
+  return s[0]+s[1:].replace(s[0],'*')
 
 
 # D. MixUp
@@ -61,7 +65,7 @@ def fix_start(s):
 # Assume a and b are length 2 or more.
 def mix_up(a, b):
   # +++your code here+++
-  return
+  return b[:2]+a[2:]+' '+a[:2]+b[2:]
 
 
 # Provided simple test() function used in main() to print
